81b38a9f7f4b8878877fbad0076a89dd399b82da
LuoTianyi712 <1228478412@qq.com>
2021-01-26 13:07:10 +0800

    多对一查询 按照查询嵌套处理

--- mybatis-06/src/main/java/com/neusoft/dao/StudentMapper.java
+++ mybatis-06/src/main/java/com/neusoft/dao/StudentMapper.java
@@ -1,4 +1,10 @@
 package com.neusoft.dao;
 
+import com.neusoft.pojo.Student;
+
+import java.util.List;
+
 public interface StudentMapper {
+
+    List<Student> getAllStudent();
 }
--- mybatis-06/src/test/java/ApplicationTest.java
+++ mybatis-06/src/test/java/ApplicationTest.java
@@ -1,7 +1,12 @@
+import com.neusoft.dao.StudentMapper;
 import com.neusoft.dao.TeacherMapper;
+import com.neusoft.pojo.Student;
 import com.neusoft.pojo.Teacher;
 import com.neusoft.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
 
 public class ApplicationTest {
     public static void main(String[] args) {
@@ -11,6 +16,21 @@ public class ApplicationTest {
         Teacher teacher = mapper.getTeacherById(1);
         System.out.println(teacher);
 
+        sqlSession.close();
+    }
+
+    @Test
+    public void testStudent() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+
+        List<Student> studentList = mapper.getAllStudent();
+
+        System.out.println("----------------------");
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+        System.out.println("----------------------");
         sqlSession.close();
     }
 }
